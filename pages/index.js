86818025e6c1908f7f1d97368fee4f6af84b6f6e
Thomas Emilsson <thomasemilsson10@gmail.com>
2019-02-12 17:56:45 +0100

    Fetching Data for Pages

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import Layout from '../components/MyLayout'
 import React from "react";
 import Link from 'next/link'
-
+import fetch from 'isomorphic-unfetch'
 
 // const PostLink = (props) => (
 //     <li>
@@ -13,23 +13,50 @@ import Link from 'next/link'
 // )
 // --> <PostLink title="Hello Next.js"/>
 
-const PostLink = (props) => (
-    <li>
-        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
+
+const Index = (props) => (
+    <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(({show}) => (
+                <li key={show.id}>
+                    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </Layout>
 )
 
-const Index = () => (
-  <Layout>
-      <h1>My Blog</h1>
-      <ul>
-          <PostLink id="hello-nextjs" title="Hello Next.js"/>
-          <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
-          <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
-      </ul>
-  </Layout>
-)
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json()
+
+    console.log(`Show data fetched. Count: ${data.length}`)
+
+    return {
+        shows: data
+    }
+}
+
+// const PostLink = (props) => (
+//     <li>
+//         <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+//             <a>{props.title}</a>
+//         </Link>
+//     </li>
+// )
+//
+// const Index = () => (
+//   <Layout>
+//       <h1>My Blog</h1>
+//       <ul>
+//           <PostLink id="hello-nextjs" title="Hello Next.js"/>
+//           <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+//           <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+//       </ul>
+//   </Layout>
+// )
 
 export default Index
