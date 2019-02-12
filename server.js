f61ea2f0748a8fa547dea511926b5f5f4c60f678
Thomas Emilsson <thomasemilsson10@gmail.com>
2019-02-12 20:28:31 +0100

    starting deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next')
 
+//"start": "NODE_ENV=production node server.js"
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
